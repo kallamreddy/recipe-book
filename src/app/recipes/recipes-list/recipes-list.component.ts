c35bb1634ca128b2877051aa8cd6021e7dbfe30f
Janardhan R Kallam <kallam.jreddy@gmail.com>
2023-02-10 00:13:57 +0530

    Added components

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent {
+  recipes : Recipe[] = [
+    new Recipe('test recipe','test','https://images.pexels.com/photos/1109197/pexels-photo-1109197.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')
+  ];
+
+  constructor(){
+
+  }
+
+}
